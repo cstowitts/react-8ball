c2efa8dc0b78cdf9288f8764b88d3d93c8417b70
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-05-03 14:27:23 -0700

    fixes counting bug, completes furhter study pt 1

---
+++ src/EightBall.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+
+function EightBall({answers}){
+    //gotta destructure or access as props.answers!
+
+    const initialAnswer = {msg: "shake me" , color: "black"}
+    const initialCount = {goldenrod: 0, red: 0, green: 0};
+
+    const [answer, setAnswer] = useState(initialAnswer);
+    const [count, setCount] = useState(initialCount);
+
+    function pickAnswer(event){
+        const idx = Math.floor(Math.random()*answers.length);
+        console.log("inside pickAnswer, idx:", idx);
+        console.log("inside pickAnswer, answers[idx]:", answers[idx]);
+        const newAnswer = answers[idx];
+        setAnswer(newAnswer);
+        setCount(count => ({...count, [newAnswer.color]: count[newAnswer.color]+1}));
+       //og was answer.color, but this created a lag in the count
+       //answer referred to the state before the click, not updated until
+       //leaving this fn
+    }
+
+    function resetBoth(event){
+        setCount(initialCount);
+        setAnswer(initialAnswer);
+    }
+
+
+    return (
+        <div className="EightBall" style={{display: 'flex', flexDirection: 'column'}}>
+            <button style={{backgroundColor: answer.color, borderRadius: '50%', width: '200px', color: 'white', height: '200px'}} onClick={pickAnswer}>
+                {answer.msg}
+            </button>
+            <button onClick={resetBoth}>
+                Reset your future!
+            </button>
+            <div>
+                {
+                    Object.keys(count).map(color => {
+                        return (<p key={color}>{color} : {count[color]}</p>);
+                    })
+                }
+            </div>
+
+        </div>
+    )
+
+}
+
+
+
+
+export default EightBall;
